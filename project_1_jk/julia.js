f750a356b86dcbed6ca8736eca70554cd8a14dcf
Julia Kambur <julia.kambur@cityscreen.cloud>
2026-01-25 02:16:12 +0100

    first version of calculator by Julia

---
+++ project_1_jk/julia.js
@@ -0,0 +1,21 @@
+let budget = 0;
+
+const budgetSpan = document.getElementById("value");
+const input = document.getElementById("input");
+const buttons = document.querySelectorAll(".categories button");
+
+budgetSpan.textContent = budget;
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const amount = Number(input.value);
+
+    if (!amount || amount <= 0) return;
+
+    budget += amount;
+    budgetSpan.textContent = budget;
+    input.value = "";
+  });
+});
+
+
